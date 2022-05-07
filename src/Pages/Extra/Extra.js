146bb5d1f3b2dfd8d936e55502284f64bde9a0b6
mirzasabbirgithub <mirzaasabbir@gmail.com>
2022-05-08 01:00:01 +0600

    footer redesigned

--- src/Pages/Extra/Extra.js
+++ src/Pages/Extra/Extra.js
@@ -3,14 +3,14 @@ import { Button, Col, Container, Row } from 'react-bootstrap';
 import book1 from '../../images/book1.jpeg';
 const Extra = () => {
           return (
-                    <div className='mt-5'>
-                              <Container>
+                    <div className='mt-5 '>
+                              <Container className='bg-info rounded'>
                                         <Row>
                                                   <Col>
-                                                            <img className='img-fluid' src={book1} alt="" />
+                                                            <img className='img-fluid rounded' src={book1} alt="" />
                                                   </Col>
                                                   <Col>
-                                                            <h3 className='text-secondary'>Special Offers</h3>
+                                                            <h3 className='text-white'>Special Offers</h3>
                                                             <h1 className='text-danger'>Get 30% Discount in all type of Books</h1>
                                                             <p>This offer valid till October 2022</p>
                                                             <Button>Click Here to Buy</Button>
--- src/Pages/SharedPages/Footer/Footer.js
+++ src/Pages/SharedPages/Footer/Footer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
-
+import '../Footer/Footer.css'
 const Footer = () => {
           const today = new Date();
           const year = today.getFullYear();
@@ -9,10 +9,34 @@ const Footer = () => {
                     <footer className=' mt-5 text-center text-white'>
                               <Container className='bg-dark'>
                                         <Row>
-                                                  <Col><span className='text-info'>Books Warehouse </span>  </Col>
-                                                  <Col><h5>Copyright &copy; {year}</h5></Col>
-                                                  <Col>Contact: dbtester@yahoo.com</Col>
+                                                  <Col>
+                                                            <h1 className='text-info'>Books Warehouse </h1>
+                                                            <h6>Nur Tower, Banani</h6>
+                                                            <h6>Dhaka-1212</h6>
+                                                            <h6>info@bookswarehouse.com</h6>
+                                                  </Col>
+
+
+                                                  <Col className='ownColor'>
+                                                            <h2 className='mb-3'>Support</h2>
+                                                            <h6>Orders and Return</h6>
+                                                            <h6>Shipping Infomation</h6>
+                                                            <h6>Store Location</h6>
+                                                            <h6>Partner Program</h6>
+                                                  </Col>
+
+                                                  <Col className='ownColor'>
+                                                            <h2 className='mb-3'>Information</h2>
+                                                            <h6>About</h6>
+                                                            <h6>Contact Us</h6>
+                                                            <h6>Refund Policy</h6>
+                                                            <h6>Location</h6>
+                                                  </Col>
                                         </Row>
+                                        <div className='bg-primary mx-auto'>
+                                                  <h5>Copyright &copy; {year} Reserved by Books Warehouse</h5>
+                                        </div>
+
                               </Container>
 
                     </footer>
